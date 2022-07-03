510845e9de43814fed32921131f0aed0c26f53db
EtherCD <91180333+EtherCD@users.noreply.github.com>
2022-07-03 09:59:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import *
+from sys import platform
+
+if platform == "linux" or platform == "linux2":
+    import alsaaudio
+elif platform == "win32":
+    from ctypes import cast, POINTER
+    from comtypes import CLSCTX_ALL
+    from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['POST', 'GET'])
+def index():
+    if request.method=='GET' and request.args.get("volume"):
+        volume:int = request.args.get("volume")
+        print(volume)
+        setVolume(int(volume))
+    return render_template("index.html")
+
+def setVolume(volume: int):
+    if volume >= 0 and volume <= 100:
+        if platform == "linux" or platform == "linux2":
+            m = alsaaudio.Mixer()
+            m.setvolume(volume)
+        elif platform == "win32":
+            devices = AudioUtilities.GetSpeakers()
+            interface = devices.Activate(
+                IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+            volume = cast(interface, POINTER(IAudioEndpointVolume))
+            volume.SetMasterVolumeLevel(volume, None)
+    
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')

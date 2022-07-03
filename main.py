10b5ccfb0185973b30a7749de5436eb3e0445ac1
EtherCD <91180333+EtherCD@users.noreply.github.com>
2022-07-03 16:19:14 +0300

    YEEEEES
    * More functions!

--- main.py
+++ main.py
@@ -1,12 +1,77 @@
 from flask import *
 from sys import platform
+import os
 
-if platform == "linux" or platform == "linux2":
+level_of_controll = 3
+
+def isLinuxOrWindows():
+    if platform == "linux" or platform == "linux2":
+        return True
+    elif platform == "win32":
+        return False
+
+if isLinuxOrWindows():
     import alsaaudio
-elif platform == "win32":
+    import pyautogui as pg
+elif not isLinuxOrWindows():
+    import pywinauto as pw
     from ctypes import cast, POINTER
     from comtypes import CLSCTX_ALL
     from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+ru_translate = {'ь':'m', 'а':'f', 'б':',', 'в':'d',
+       'г':'u', 'д':'l', 'е':'t', 'ё':'`', 'ж':';',
+       'з':'p', 'и':'b', 'й':'q', 'к':'r', 'л':'k',
+       'м':'v', 'н':'y', 'о':'j', 'п':'g', 'р':'h', 
+       'с':'c', 'т':'n', 'у':'e', 'ф':'a', 'х':'[',
+       'ц':'w', 'ч':'x', 'ш':'i', 'щ':'o', 'ы':'s',
+       'э':"'", 'ю':'.', 'я':'z', 'з':'p'}
+
+def translate(translate, text):
+    result=""
+    len_text=len(text)
+    for i in range(0,len_text):
+        simb=""
+        if text[i] in translate:
+            simb = translate[text[i]]
+        else:
+            simb = text[i]
+        result = result + simb
+    return result
+
+def writeRaETextLinux(text, control0="", control1="", interval=0.25):
+    current_lang=os.system("xset -q | grep LED | awk '{print $10}' | cut -c 5")
+    if bool(set(ru_translate).intersection(set(text.lower()))):
+        if current_lang:
+            pg.typewrite(text, interval=interval)
+        else:
+            if not control0:
+                pg.hotkey("shift", "alt")
+                pg.typewrite(translate(ru_translate, " "+text), interval=interval)
+                pg.hotkey("shift", "alt")
+            else:
+                pg.hotkey(control0, control1)
+                pg.typewrite(translate(ru_translate, " "+text), interval=interval)
+                pg.hotkey(control0, control1)
+    else:
+        if not current_lang:
+            pg.write(text, interval=interval)
+        else:
+            if not control0:
+                pg.hotkey("shift", "alt")
+                pg.write(text, interval=interval)
+                pg.hotkey("shift", "alt")
+            else:
+                pg.hotkey(control0, control1)
+                pg.write(text, interval=interval)
+                pg.hotkey(control0, control1)
+
+def translateKeyToWinKey(key: str):
+    if str == "up"
+    or str == "down"
+    or str == "left"
+    of str == "right": return "{VK_"+key.upper()+"}"
+    return "{"+key.upper()+"}"
 
 app = Flask(__name__)
 
@@ -14,16 +79,28 @@ app = Flask(__name__)
 def index():
     if request.method=='GET' and request.args.get("volume"):
         volume:int = request.args.get("volume")
-        print(volume)
+        print("Getted request to change Volume: "+volume)
         setVolume(int(volume))
-    return render_template("index.html", current_volume=getVolume())
+    elif request.method=='GET' and request.args.get("control"):
+        if isLinuxOrWindows():
+            pg.press(request.args.get("control"))
+        elif not isLinuxOrWindows():
+            pw.keyboard.send_keys(translateKeyToWinKey(request.args.get("control")))
+    elif request.method=='GET' and request.args.get("text_to_put"):
+        text=request.args.get("text_to_put")
+        if isLinuxOrWindows():
+            writeRaETextLinux(text, control0="win", control1="space")
+        elif not isLinuxOrWindows():
+            pw.keyboard.send_keys(request.args.get("control"))
+    return render_template("index.html", current_volume=getVolume(), level_of_controll=level_of_controll)
+
 
 def setVolume(volume: int):
     if volume >= 0 and volume <= 100:
-        if platform == "linux" or platform == "linux2":
+        if isLinuxOrWindows():
             m = alsaaudio.Mixer()
             m.setvolume(volume)
-        elif platform == "win32":
+        elif not isLinuxOrWindows():
             devices = AudioUtilities.GetSpeakers()
             interface = devices.Activate(
                 IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
@@ -31,7 +108,7 @@ def setVolume(volume: int):
             volume.SetMasterVolumeLevel(volume, None)
     
 def getVolume():
-    if platform == "linux" or platform == "linux2":
+    if isLinuxOrWindows():
         m=alsaaudio.Mixer()
         return m.getvolume()[0]
     return 0

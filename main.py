8df8ba1a076a9d25b4f8c0fe6df98f74a936abc5
EtherCD <91180333+EtherCD@users.noreply.github.com>
2022-07-21 04:08:27 +0300

    Update 1.1.5 (With Errors)
    It seems to me that I have an error with not loading the page. Probably no.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import *
 from sys import platform
 import pyautogui as pg
+import json
 import os
 
 level_of_controll = 3
@@ -18,6 +19,9 @@ elif not isLinuxOrWindows():
     import pywinauto as pw
     from Sound import Sound
 
+def getAllTranslates():
+    return os.listdir("./translates")
+
 #This is a translation for the output of Russian and other languages for Linux
 #If necessary, you can replace the signs of your language in turn, the same letter is only in English
 ru_translate = {'ь':'m', 'а':'f', 'б':',', 'в':'d',
@@ -29,6 +33,8 @@ ru_translate = {'ь':'m', 'а':'f', 'б':',', 'в':'d',
        'э':"'", 'ю':'.', 'я':'z', 'з':'p'}
 
 app = Flask(__name__)
+
+translate = {}
 
 @app.route("/", methods = ['POST', 'GET'])
 def index():
@@ -124,4 +130,36 @@ def translateKeyToWinKey(key: str):
     return "{"+key.upper()+"}"
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    if translate != {}:
+        s = False
+        if not os.path.isfile("./settings.json"):
+            with open("settings.json", "r+") as f:
+                if f.read() == "":
+                    s=True
+        if s:
+            translates = getAllTranslates()
+            print("Set Language:")
+            for translate in translates:
+                with open("./translates/"+translate, encoding="utf-8") as f:
+                    translateF=json.load(f)
+                    print(translate+":"+translateF["Name"])
+            inp = input("Put Translate Name(en):")
+            if inp+".json" in translates:
+                with open("./translates/"+inp+".json", encoding="utf-8") as f:
+                    translateF=json.load(f)
+                    translate=translateF
+                with open("settings.json", "w+") as f:
+                    f.write('{"currentT":"'+inp+'"}')
+                print("Setted!")
+                app.run(debug=True, host='0.0.0.0')
+            print("This not option")
+        else:
+            with open("settings.json", "r+") as f:
+                ct = json.load(f)
+            if ct["currentT"] != "":
+                with open("./translates/"+ct["currentT"]+".json", encoding="utf-8") as f:
+                    translateF=json.load(f)
+                    translate=translateF
+            app.run(debug=True, host='0.0.0.0')
+    else:
+        app.run(debug=True, host='0.0.0.0')
